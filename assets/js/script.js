1b66e027082058d7261ad651d7e6523ed8edcb17
Maxim <63422884+MaximQu@users.noreply.github.com>
2022-09-28 14:59:24 +0300

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-import burgerMenu from './modules/burger.js';
-import headerSticky from './modules/header-sticky.js';
-import faq from './modules/faq.js';
-
-headerSticky();
-burgerMenu();
-faq();
